77621ab80ee32ef521c052efc3809c592b6a9bd0
AkgaCoolCode <168640847+AkgaCoolCode@users.noreply.github.com>
2025-04-01 20:00:36 +0200

    The game is doooooooooooooooooooooooooooooooooooooooooooooone

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,12 +10,16 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private Players playerLeft;
     [SerializeField] private Players playerRight;
+    [SerializeField] private TMP_Text timerText;
+
+    private int timer = 100;
 
     // Start is called before the first frame update
     void Start()
     {
         if (Instance == null) Instance = this;
         else Destroy(gameObject);
+        InvokeRepeating(nameof(CountDown), 1, 1);
     }
 
     // Update is called once per frame
@@ -22,9 +28,23 @@ public class GameManager : MonoBehaviour
         
     }
 
+
     public void Goal()
     {
         playerLeft.Reset();
         playerRight.Reset();
+    }
+
+
+    private void CountDown()
+    {
+        timer--;
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene("EndGame");
+        }
+       
+        timerText.text = Helpers.FormatTimer(timer);
+
     }
 }
